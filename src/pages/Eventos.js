b0047a324499604d7f3cca7ad790fcdd573ab8c7
Lucía <luciarol87@gmail.com>
2024-11-05 19:45:46 +0100

    Modificado css de eventos: añadido grid responsive

--- src/pages/Eventos.js
+++ src/pages/Eventos.js
@@ -9,47 +9,46 @@ import '../styles/eventos.css'; // Import CSS for styles
       id: 1,
       title: 'Miércoles Dragoneros',
       img: event1,
-      description: 'Nos reunimos por las tardes para jugar a lo que más nos apetezca ¡Pide tu plaza en el grupo de whatsapp y pásate! ¡Tenemos ganas de conocerte! :)',
+      description: 'Nos reunimos por las tardes para jugar a lo que más nos apetezca...',
       date: 'Todos los miércoles por la tarde',
     },
     {
       id: 2,
       title: 'Noche de Halloween',
       img: event2,
-      description: 'Una vez al año, nos reunimos para jugar a juegos de roles ocultos, asesinatos y misterios. ¿Te atreves a pasarte a jugar?.',
+      description: 'Una vez al año, nos reunimos para jugar a juegos de roles ocultos...',
       date: 'Noche del 31 de Octubre, 2024',
     },
     {
       id: 3,
       title: 'Reunión Familiar de Juegos',
       img: event3,
-      description: 'Un evento para toda la familia. Ven y disfruta de juegos clásicos y modernos con amigos, familiares y miembros del club.',
+      description: 'Un evento para toda la familia...',
       date: '30 de Octubre, 2024',
     },
-    // Add more events as needed
   ];
-
+  
   const Event = ({ event }) => {
     return (
       <div className="event-container">
-        <img src={event.img} alt={event.title} style={{ width: '300px', height: '200px' }} />
+        <img src={event.img} alt={event.title} />
         <h3>{event.title}</h3>
         <p>{event.description}</p>
         <p><strong>Fecha:</strong> {event.date}</p>
       </div>
     );
   };
-
+  
   const Eventos = () => {
     return (
       <div>
         <h1>Eventos</h1>
         <div className="description">
           <p>
-            Estos son eventos especiales que aprovechamos para reunirnos y pasar un buen rato todos juntos. No seáis tímid@s y pasaros a conocernos.
+            Eventos especiales que aprovechamos para reunirnos y pasar un buen rato todos juntos...
           </p>
         </div>
-        <div className="events-list" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        <div className="events-list">
           {events.map(event => (
             <Event key={event.id} event={event} />
           ))}
@@ -57,5 +56,5 @@ import '../styles/eventos.css'; // Import CSS for styles
       </div>
     );
   };
-
+  
   export default Eventos;

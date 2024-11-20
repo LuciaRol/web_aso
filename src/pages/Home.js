2c0ea1ce6feebea38cdcfbbfb95c8183a1b21981
luciarol <luciarol87@gmail.com>
2024-11-20 18:47:15 +0100

    commit limpieza home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,64 +4,6 @@ import partidaAbierta from '../img/dragon_dnd.jpg';
 import '../styles/home.css';
 
 const Home = () => {
-  // const [showChat, setShowChat] = useState(false);
-  // const [questionCount, setQuestionCount] = useState(0);
-  // const MAX_QUESTIONS = 10;
-
-  // const handleEnd = () => {
-  //   alert('La conversación ha terminado. ¡Gracias por usar nuestro chatbot!');
-  // };
-
-  // const steps = [
-  //   {
-  //     id: '1',
-  //     message: '¡Hola! Soy un asistente virtual del Club de Juegos de Mesa. ¿En qué te puedo ayudar?',
-  //     trigger: '2',
-  //   },
-  //   {
-  //     id: '2',
-  //     user: true,
-  //     trigger: ({ value }) => {
-  //       if (questionCount < MAX_QUESTIONS - 1) {
-  //         setQuestionCount(questionCount + 1);
-  //         return '3';
-  //       } else {
-  //         return 'end';
-  //       }
-  //     },
-  //   },
-  //   {
-  //     id: '3',
-  //     message: ({ previousValue }) => {
-  //       const response = getChatbotResponse(previousValue);
-  //       return response;
-  //     },
-  //     trigger: '4',
-  //   },
-  //   {
-  //     id: '4',
-  //     message: '¿Tienes otra pregunta?',
-  //     trigger: '5',
-  //   },
-  //   {
-  //     id: '5',
-  //     options: [
-  //       { value: 'yes', label: 'Sí', trigger: '2' },
-  //       { value: 'no', label: 'No', trigger: 'end' },
-  //     ],
-  //   },
-  //   {
-  //     id: 'end',
-  //     message: '¡Gracias por tus preguntas! Si tienes más dudas, no dudes en preguntar más tarde.',
-  //     end: true,
-  //   },
-  // ];
-
-  // const toggleChat = () => {
-  //   setShowChat(!showChat);
-  //   setQuestionCount(0); // Reiniciar contador de preguntas al abrir el chat
-  // };
-
   return (
     <div className="home-container">
       <div className="background-container">
@@ -88,34 +30,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className={`chatbot-container ${showChat ? 'active' : ''}`}>
-        <div className="chatbot-header">
-          <button className="chatbot-close-button" onClick={toggleChat}>
-            &times;
-          </button>
-        </div>
-        <ChatBot
-          steps={steps}
-          handleEnd={handleEnd}
-          headerTitle={
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <img src={nuevoLogo} alt="Logo" style={{ width: '30px', height: '30px', marginRight: '10px' }} />
-              <span>Chatbot</span>
-            </div>
-          }
-        />
-      </div>
-
-      {!showChat && (
-        <div className="floating-chat-button">
-          <button onClick={toggleChat} className="fancy-chat-toggle-button">
-            <img src={meepleImage} alt="Meeple" className="meeple-icon" />
-            ¡Pregunta al chatbot!
-          </button>
-        </div>
-      )} */}
-    </div>
+   </div>
   );
 };
 

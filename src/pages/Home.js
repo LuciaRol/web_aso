c9cb6853c5868883e2c236bd693cc515aac43d4c
Lucía <luciarol87@gmail.com>
2024-12-24 18:23:20 +0100

    reinicio landing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,28 @@
 import React, { useState } from 'react';
 import Counter from '../components/counter';
 import partidaVideo from '../img/dragon_dnd.mp4'; // Ruta del video
+import logoNegro from '../img/landing/logo_negro_transp.png';
+import queenDragon from '../img/landing/queen_dragon.png';
 import '../styles/home.css';
 
 const Home = () => {
-  const [isVideoReady, setIsVideoReady] = useState(false);
-
-  // Maneja el cambio de estado cuando el video está listo
-  const handleVideoReady = () => {
-    setIsVideoReady(true);
-  };
+  
 
   return (
     <div className="home-container">
-      {/* Mostrar el video de fondo en bucle */}
-      <video
-        className="home-video"
-        autoPlay
-        muted
-        loop={true} // El video se reproduce en bucle
-        onCanPlay={handleVideoReady}
-        style={{ display: isVideoReady ? 'block' : 'none' }} // Ocultar hasta que esté listo
-      >
-        <source src={partidaVideo} type="video/mp4" />
-        Tu navegador no soporta videos HTML5.
-      </video>
+      <div>
+        <img src={queenDragon}/>
+      </div>
 
-      {/* Texto superpuesto */}
-      {isVideoReady && (
-        <div className="overlay-text">
-          <h1>Dragón de Madera</h1>
-          <p>Tu asociación de juegos de mesa, rol, wargames y miniaturas de Granada.</p>
 
-          {/* Sección de contadores */}
-          <div className="counter-section">
-            {/* Cuadrado del contador de juegos */}
-            <div className="counter-box">
-              <h2 className="counter-title">Juegos</h2>
-              <Counter endValue={900} />
-            </div>
-
-            {/* Cuadrado del contador de partidas */}
-            <div className="counter-box">
-              <h2 className="counter-title">Partidas</h2>
-              <Counter endValue={300} />
-            </div>
-          </div>
-        </div>
-      )}
+     
     </div>
+
+
+
   );
+
+  
 };
 
 export default Home;

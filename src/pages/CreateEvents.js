496d7a432513cfb59ceb420b934bb5dc23644fff
luciarol <luciarol87@gmail.com>
2024-12-29 11:23:18 +0100

    editar eventos

--- src/pages/CreateEvents.js
+++ src/pages/CreateEvents.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs, addDoc, deleteDoc, doc, query, where } from 'firebase/firestore';
+import { collection, getDocs, addDoc, deleteDoc, doc, query, where, updateDoc } from 'firebase/firestore';
 import Modal from '../components/Modal';
 import '../styles/events.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -20,6 +20,7 @@ const CreateEvents = () => {
   const [loading, setLoading] = useState(false);
   const [usuario] = useAuthState(auth);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [editEvent, setEditEvent] = useState(null);
   const thread_id = 180; // Id del tema Eventos
 
   const fetchEvents = async () => {
@@ -53,6 +54,10 @@ const CreateEvents = () => {
     }
   }, [usuario]);
 
+  const handleEditEvent = (event) => {
+    setEditEvent(event);
+  };
+  
   const checkIfUserIsAdmin = async (email) => {
     try {
       const querySnapshot = await getDocs(
@@ -127,6 +132,42 @@ const CreateEvents = () => {
     } catch (error) {
       toast.error('Error al eliminar el evento.');
       console.error('Error al eliminar el evento:', error);
+    }
+  };
+
+  const handleUpdateEvent = async (e) => {
+    e.preventDefault();
+  
+    if (!isAdmin) {
+      toast.error('No tienes permisos para editar eventos.');
+      return;
+    }
+  
+    if (!editEvent.titulo || !editEvent.fecha || !editEvent.hora || !editEvent.descripcion || !editEvent.imagen) {
+      toast.error('Por favor, completa todos los campos.');
+      return;
+    }
+    
+    setLoading(true);
+    try {
+      await updateDoc(doc(firestore, 'eventos', editEvent.id), {
+        titulo: editEvent.titulo,
+        fecha: editEvent.fecha,
+        hora: editEvent.hora,
+        descripcion: editEvent.descripcion,
+        imagen: editEvent.imagen,
+      });
+  
+      toast.success('Evento actualizado correctamente.');
+
+
+      fetchEvents();  // Refresca la lista de eventos después de la actualización
+      setEditEvent(null);  // Resetea el estado del evento editado
+    } catch (error) {
+      toast.error('Error al actualizar el evento.');
+      console.error('Error al actualizar el evento:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -225,11 +266,10 @@ const CreateEvents = () => {
             </p>
             <button 
               onClick={(e) => {
-                e.stopPropagation(); // Prevent modal from opening
-                // Add functionality for this button as needed
-                console.log('New button clicked for event:', event.id);
+                e.stopPropagation();  // Evita que se abra el modal al hacer clic
+                handleEditEvent(event);  // Llama a la función para editar
               }} 
-              className="delete-button" // Using the same styling class
+              className="edit-button"
             >
               Editar Evento
             </button>
@@ -246,6 +286,73 @@ const CreateEvents = () => {
           </div>
         ))}
       </div>
+      {editEvent && (
+        <div className="edit-form">
+          <h3>Editar Evento</h3>
+          <form onSubmit={handleUpdateEvent}>
+            <div className="form-group">
+              <label htmlFor="titulo">Título</label>
+              <input
+                id="titulo"
+                className="form-control"
+                type="text"
+                value={editEvent.titulo}
+                onChange={(e) => setEditEvent({ ...editEvent, titulo: e.target.value })}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="fecha">Fecha</label>
+              <input
+                id="fecha"
+                className="form-control"
+                type="date"
+                value={editEvent.fecha}
+                onChange={(e) => setEditEvent({ ...editEvent, fecha: e.target.value })}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="hora">Hora</label>
+              <input
+                id="hora"
+                className="form-control"
+                type="time"
+                value={editEvent.hora}
+                onChange={(e) => setEditEvent({ ...editEvent, hora: e.target.value })}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="descripcion">Descripción</label>
+              <textarea
+                id="descripcion"
+                className="form-control"
+                value={editEvent.descripcion}
+                onChange={(e) => setEditEvent({ ...editEvent, descripcion: e.target.value })}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="imagen">URL de la Imagen</label>
+              <input
+                id="imagen"
+                className="form-control"
+                type="url"
+                value={editEvent.imagen}
+                onChange={(e) => setEditEvent({ ...editEvent, imagen: e.target.value })}
+                required
+              />
+            </div>
+
+            <button type="submit">Actualizar Evento</button>
+          </form>
+        </div>
+      )}
 
       {selectedEvent && (
         <Modal isOpen={isModalOpen} onClose={closeModal} event={selectedEvent} />

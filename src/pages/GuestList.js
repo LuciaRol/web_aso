257f1f148035d59e42a647945ba1c6cbc87e0874
Lucía <luciarol87@gmail.com>
2025-01-07 11:38:34 +0100

    paginación en lista de invitados funcionando

--- src/pages/GuestList.js
+++ src/pages/GuestList.js
@@ -7,6 +7,8 @@ import '../styles/guestlist.css';
 const GuestList = () => {
   const [guestData, setGuestData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const [itemsPerPage] = useState(5); // Número de registros por página
 
   // Función para obtener los datos de los invitados desde Firestore
   const fetchGuestData = async () => {
@@ -65,63 +67,93 @@ const GuestList = () => {
     XLSX.writeFile(workbook, "GuestList.xlsx");
   };
 
+  // Función para cambiar la página
+  const changePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   // Obtener los datos al montar el componente
   useEffect(() => {
     fetchGuestData();
   }, []);
+
+  // Lógica para la paginación
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentGuests = guestData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Número total de páginas
+  const totalPages = Math.ceil(guestData.length / itemsPerPage);
 
   return (
     <div className='guest-list-container'>
       <h1>Lista de Invitados</h1>
 
       {/* Botón para exportar los datos a Excel */}
-        <div className="button-container">
-            <button onClick={exportToExcel} className="export-button submit-button">
-                Exportar a Excel
-            </button>
-        </div>
+      <div className="button-container">
+        <button onClick={exportToExcel} className="export-button submit-button">
+          Exportar a Excel
+        </button>
+      </div>
 
       {/* Tabla de resultados */}
       <div className="results-container--unique">
-      {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        <div className="guest-list--unique">
-          {guestData.length > 0 ? (
-            guestData.map((guest, index) => (
-              <div key={index} className="guest-row--unique">
-              <div className="guest-card__row--unique">
-                <span className="guest-card__label--unique">Nombre:</span>
-                <span className="guest-card__value--unique">{guest.name}</span>
-              </div>
+        {loading ? (
+          <p>Cargando...</p>
+        ) : (
+          <div className="guest-list--unique">
+            {currentGuests.length > 0 ? (
+              currentGuests.map((guest, index) => (
+                <div key={index} className="guest-row--unique">
+                  <div className="guest-card__row--unique">
+                    <span className="guest-card__label--unique">Nombre:</span>
+                    <span className="guest-card__value--unique">{guest.name}</span>
+                  </div>
 
-              <div className="guest-card__row--unique">
-                <span className="guest-card__label--unique">Apellidos:</span>
-                <span className="guest-card__value--unique">{guest.surname}</span>
-              </div>
+                  <div className="guest-card__row--unique">
+                    <span className="guest-card__label--unique">Apellidos:</span>
+                    <span className="guest-card__value--unique">{guest.surname}</span>
+                  </div>
 
-              <div className="guest-card__row--unique">
-                <span className="guest-card__label--unique">Fecha de las visitas:</span>
-                <div className="guest-card__value--unique">
-                  {guest.visits.length > 0 ? (
-                    guest.visits.map((visit, idx) => (
-                      <div key={idx}>{visit}</div>
-                    ))
-                  ) : (
-                    <div>No tiene visitas registradas.</div>
-                  )}
+                  <div className="guest-card__row--unique">
+                    <span className="guest-card__label--unique">Fecha de las visitas:</span>
+                    <div className="guest-card__value--unique">
+                      {guest.visits.length > 0 ? (
+                        guest.visits.map((visit, idx) => (
+                          <div key={idx}>{visit}</div>
+                        ))
+                      ) : (
+                        <div>No tiene visitas registradas.</div>
+                      )}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              ))
+            ) : (
+              <p>No se encontraron invitados.</p>
+            )}
+          </div>
+        )}
+      </div>
 
-            ))
-          ) : (
-            <p>No se encontraron invitados.</p>
-          )}
-        </div>
-  )}
-</div>
-
+      {/* Controles de paginación */}
+      <div className="pagination-container">
+        <button
+        className='submit-button guest-button'
+          onClick={() => changePage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Anterior
+        </button>
+        <span>Página {currentPage} de {totalPages}</span>
+        <button
+          className='submit-button guest-button'
+          onClick={() => changePage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Siguiente
+        </button>
+      </div>
     </div>
   );
 };
